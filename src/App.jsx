fcceedfbc8447ba8862a302e3a74119f284b93ce
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-02-18 12:28:26 +0200

    Add crypto prices

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import LetterCanvas from "./components/LetterCanvas";
+import { data } from "./mock/data";
 
 function App() {
   return (
@@ -11,15 +12,44 @@ function App() {
         <LetterCanvas />
       </div>
 
-      <div className="bg-gray-900 h-[50vh]">
-        <div className="max-w-[800px] mx-auto flex flex-col items-center">
-          <div className="mt-28 flex flex-col gap-20 items-center">
-            <h1 className="text-white text-5xl font-semibold tracking-wider text-center">
+      <div className="bg-gray-900 min-h-[80vh]">
+        <div className="max-w-[1000px] mx-auto flex flex-col items-center">
+          <div className="flex flex-col items-center gap-20 mt-28">
+            <h1 className="text-xl font-semibold tracking-wider text-center text-white md:text-5xl">
               Buy Bitcoin, Ethereum, and 250+ cryptocurrencies
             </h1>
-            <button className="text-white border rounded-3xl w-[35%] p-3 hover:bg-gray-800 transition duration-300 ease-in-out">
+            <button className="text-white border rounded-3xl md:w-[35%] p-3 hover:bg-gray-800 transition duration-300 ease-in-out">
               Check crypto prices
             </button>
+            <div className="grid w-full gap-8 px-2 md:px-0">
+              {data.map((coin, index) => (
+                <div
+                  key={index}
+                  className="grid grid-cols-4 gap-4 md:grid-cols-12 md:gap-8 w-full md:w-[90%] mx-auto border-b-[0.5px] py-2"
+                >
+                  <div className="col-span-2 md:col-span-3">
+                    <h1 className="text-lg font-semibold tracking-wider text-gray-300 md:text-2xl">
+                      {coin.name}
+                    </h1>
+                  </div>
+                  <div className="col-span-2 md:col-span-3">
+                    <h1 className="text-lg font-bold tracking-wider text-white md:text-3xl text-end md:text-center">
+                      {coin.price}
+                    </h1>
+                  </div>
+                  <div className="col-span-2 md:col-span-3">
+                    <h1 className="text-lg font-semibold tracking-wider text-green-500 md:text-xl md:text-center">
+                      {coin.change}
+                    </h1>
+                  </div>
+                  <div className="col-span-2 md:col-span-3 text-end md:text-end">
+                    <h1 className="text-lg font-semibold tracking-wider text-white md:text-xl">
+                      {coin.marketCap}
+                    </h1>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
---
+++ src/mock/data.js
@@ -0,0 +1,7 @@
+export const data = [
+  { name: "Bitcoin", price: "$60,000", change: "1.5%", marketCap: "1.2T" },
+  { name: "Ethereum", price: "$3,000", change: "2.5%", marketCap: "400B" },
+  { name: "Binance Coin", price: "$600", change: "3.5%", marketCap: "100B" },
+  { name: "Cardano", price: "$2.5", change: "4.5%", marketCap: "100B" },
+  { name: "Dogecoin", price: "$0.5", change: "5.5%", marketCap: "50B" },
+];
